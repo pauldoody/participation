3205bf5768fdf3e745413e5e7615bcf18dbc3cd2
pauldoody <pdoody@gmu.edu>
2020-03-24 19:00:02 -0400

    ummm

--- src/main/java/servlet/twoButtons.java
+++ src/main/java/servlet/twoButtons.java
@@ -33,8 +33,8 @@ public class twoButtons extends HttpServlet
 {
 
 // Location of servlet.
-static String Domain  = "cs.gmu.edu:8443";
-static String Path    = "/offutt/servlet/";
+static String Domain  = "pauldoodyparticipation.herokuapp.com";
+static String Path    = "/twoButtons";
 static String Servlet = "twoButtons";
 
 // Button labels

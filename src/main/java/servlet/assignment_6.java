6e5f881eacb6a0fd8a315262a96336299676c76e
pauldoody <pdoody@gmu.edu>
2020-03-25 15:44:26 -0400

    yes

--- src/main/java/servlet/assignment_6.java
+++ src/main/java/servlet/assignment_6.java
@@ -56,30 +56,7 @@ static String Style ="https://www.cs.gmu.edu/~offutt/classes/432/432-style.css";
 public void doPost (HttpServletRequest request, HttpServletResponse response)
    throws ServletException, IOException
 {
-   Float rslt   = new Float(0.0);
-   Float lhsVal = new Float(0.0);
-   Float rhsVal = new Float(0.0);
-   String operation = request.getParameter("Operation");
-   String lhsStr = request.getParameter("LHS");
-   String rhsStr = request.getParameter("RHS");
-   if ((lhsStr != null) && (lhsStr.length() > 0))
-      lhsVal = new Float(lhsStr);
-   if ((rhsStr != null) && (rhsStr.length() > 0))
-      rhsVal = new Float(rhsStr);
-
-   if (operation.equals(OperationAdd))
-   {
-      rslt = new Float(lhsVal.floatValue() + rhsVal.floatValue());
-   }
-   else if (operation.equals(OperationSub))
-   {
-      rslt = new Float(lhsVal.floatValue() - rhsVal.floatValue());
-   }
-   else if (operation.equals(OperationMultiply))
-   {
-	   rslt = new Float(lhsVal.floatValue() * rhsVal.floatValue());
-   }
-
+	
    response.setContentType("text/html");
    PrintWriter out = response.getWriter();
    PrintHead(out);
@@ -190,7 +167,7 @@ out.println("</p>");
 out.println("</td></tr>");
 out.println("</table>");
 out.println("<b>");
-out.println("<form method=\"post\" action=\"https://pauldoodyparticipation.herokuapp.com/assignment_6\" name=\"my_form\" onSubmit=\"return SubmissionTest()\")>");
+out.println("<form method=\"post\" action=\"pauldoodyparticipation.herokuapp.com/assignment_6\" name=\"my_form\" onSubmit=\"return SubmissionTest()\")>");
 
 
 out.println("<table border = 1 align = \"center\" id = \"radio_table\">");

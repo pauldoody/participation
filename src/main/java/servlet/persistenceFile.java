bbb96bd607b6f3454721a5fa93a6b661a9a4c896
pauldoody <pdoody@gmu.edu>
2020-04-14 17:12:42 -0400

    yep

--- src/main/java/servlet/persistenceFile.java
+++ src/main/java/servlet/persistenceFile.java
@@ -19,15 +19,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(name = "FilePersistence", urlPatterns = {"/file"})
+@WebServlet(name = "persistance", urlPatterns = {"/persistance"})
 public class persistenceFile extends HttpServlet{
   static enum Data {AGE, NAME};
   static String RESOURCE_FILE = "entries.txt";
   static final String VALUE_SEPARATOR = ";";
 
-  static String Domain  = "";
+  static String Domain  = "pauldoodyparticipation.herokuapp.com";
   static String Path    = "/";
-  static String Servlet = "file";
+  static String Servlet = "participation";
 
   // Button labels
   static String OperationAdd = "Add";

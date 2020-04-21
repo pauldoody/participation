e350ef142c2cf5f0c28e84a3d74d363659956723
pauldoody <pdoody@gmu.edu>
2020-04-21 18:30:37 -0400

    indeed

--- src/main/java/servlet/attributeServlet.java
+++ src/main/java/servlet/attributeServlet.java
@@ -8,6 +8,14 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.util.Enumeration;
 
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
 @WebServlet(name = "attribute", urlPatterns = {"/attribute"})
 public class attributeServlet extends HttpServlet
 {
